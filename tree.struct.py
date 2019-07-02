a52d056b9e66b9892fd18109f2b2e134764d37b3
Yasir Nadeem <m-yasir.dev@protonmail.com>
2019-07-03 03:16:29 +0500

    Implemented folder search, getfolderchildren and printfolderchildren methods along. Added comments to almost all the methods.

--- tree.struct.py
+++ tree.struct.py
@@ -6,14 +6,38 @@ class Tree:
         self.string = ""
         return
 
+    # A Public access member method to find the required folder
     def getFolder(self, folderName):
-        
-        return
+        return self._folderSearch(self.root, folderName)
     
+    # A Helper private member method to find the required folder that requires the root as well as the folder name
+    def _folderSearch(self, root, folderName):
+        if root is None:
+            return root
+        
+        if root.name == folderName:
+            return root
+        folder = None
+        for next in root.nexts:
+            folder = self._folderSearch(next, folderName)
+            if folder:
+                return folder
+        return folder
+
+    # A public member method to get the children folders/files of the folder passed in
+    def getFolderChildren(self, folder):
+        return folder.getChildren()
+
+    # A public member method to print all the folders
     def printFolders(self):
         self._populateString(self.root)
         print(self.string)
         self.string = ""
+        return
+    # A public member method to print all the nested children folder names including the parent folder
+    def printFolderChildrens(self, folder):
+        self._populateString(folder)
+        print(self.string)
         return
     # a private method used to fill string with all the folder names
     def _populateString(self, myRoot):
@@ -41,8 +65,21 @@ class FolderNode:
         myFolder = FolderNode(name)
         self.nexts.append(myFolder)
         return
+    
+    def getChildren(self):
+        return self.nexts
+
+# ----------------- TESTING METHODS BELOW --------------------
 
 directory = Tree()
 directory.root.createFolder("first")
 directory.root.createFolder("second")
-directory.printFolders()
+
+folder = directory.getFolder("first")
+
+directory.printFolderChildrens(folder)
+# print(folder.name if folder else "Folder Not Found!")
+
+# directory.printFolders()
+
+# ------------------ TEST ENDS HERE ---------------------------
